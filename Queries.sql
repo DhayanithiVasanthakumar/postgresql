432da4cef7bd7bc184bdf3136c3aceed725e67a3
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-09-13 14:43:49 +0530

    table

--- Queries.sql
+++ Queries.sql
@@ -2,8 +2,23 @@
 CREATE TABLE
 */
 CREATE TABLE students(
-	s_no INT,
+	s_no INT NOT NULL,
 	student_name VARCHAR(50), 
 	student_regno INT
 );
+
+/*
+INSERT VALUES INTO TABLE
+*/
+
+INSERT INTO students(s_no,student_name,student_regno)
+VALUES(1,'dhaya',08),
+(2,'aswini',07),
+(3,'naren',13);
+
+/*
+DISPLAY RECORDS
+*/
+
+SELECT * FROM students;
 
